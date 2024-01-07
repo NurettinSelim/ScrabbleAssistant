f15027fa5883b401fb828ef42768815a182419ab
Nurettin <nurettinselim03@gmail.com>
2024-01-07 09:37:39 +0300

    messages updated

--- src/tr/edu/ozyegin/cs101/scrabble/Main.java
+++ src/tr/edu/ozyegin/cs101/scrabble/Main.java
@@ -11,12 +11,14 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        System.out.println("Enter your hand. At most seven tiles, use _ for blank tiles.");
         String letters = scanner.nextLine().toUpperCase();
         ScrabbleChar[] scrabbleChars = new ScrabbleChar[letters.length()];
         for (int i = 0; i < letters.length(); i++) {
             scrabbleChars[i] = new ScrabbleChar(letters.charAt(i));
         }
+
+        System.out.println("Enter the pattern to match. Use * for tiles you will place.");
         String pattern = scanner.nextLine().toUpperCase();
 
         String[] dict = WordReader.readWords("/Users/selim/IdeaProjects/ScrabbleAssistant/src/tr/edu/ozyegin/cs101/scrabble/dictionary.txt");
@@ -35,7 +37,7 @@ public class Main {
 
         Collections.sort(validWords);
         for (Word validWord : validWords) {
-            System.out.println("Word: " + validWord.getWord() + " --- Point of word: " + validWord.getPoint());
+            System.out.println(validWord.getPoint() + ": " + validWord.getWord());
         }
     }
 
